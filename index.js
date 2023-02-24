5df44c7bcd7be4bfc1606caad0d4f666ea9c0e47
den <denissytnikmacho@gmail.com>
2023-02-24 14:55:25 +0200

    feature: made function for handling interactive mod

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+
+const interactiveMode = () => {
+  const params = [];
+  const questions = ["a = ", "b = ", "c = "];
+
+  process.stdin.on("data", (data) => {
+    if (params.length < questions.length) {
+      const num = parseFloat(data.toString());
+      if (!checkNum(data)) {
+        console.log(`Error. Expected a valid real number, got ${data} instead`);
+        process.stdout.write(questions[params.length]);
+      } else if (num === 0) {
+        console.log(`Error. a cannot be 0`);
+        process.stdout.write(questions[params.length]);
+      } else {
+        params[params.length] = num;
+        if (params.length < questions.length) {
+          process.stdout.write(questions[params.length]);
+        } else {
+          solver(...params);
+          process.exit(0);
+        }
+      }
+    }
+  });
+
+  process.stdout.write(questions[0]);
+};
+
 const solver = (a, b, c) => {
   console.log(`The equation is: (${a})x^2 + (${b})x + (${c}) = 0`);
   const discriminant = b * b - 4 * a * c;
