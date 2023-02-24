709a731fc285a5ca98805a130daf80c0a1a40b50
den <denissytnikmacho@gmail.com>
2023-02-24 15:11:02 +0200

    feature: added mode switcher

--- index.js
+++ index.js
@@ -71,3 +71,9 @@ const solver = (a, b, c) => {
     return console.log(`x1 = ${x1.toFixed(2)}\nx2 = ${x2.toFixed(2)}`);
   }
 };
+
+if (!filePath) {
+  interactiveMode();
+} else {
+  nonInteractiveMod();
+}
